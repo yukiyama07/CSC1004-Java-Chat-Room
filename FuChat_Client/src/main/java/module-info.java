b5258936d062d02d72456c30d0a0037067ff5517
Huo Yu <112453676+yukiyama07@users.noreply.github.com>
2023-04-23 14:25:23 +0800

    Delete FuChat_Client directory

--- FuChat_Client/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module FuChat.Client {
-    requires javafx.graphics;
-    requires javafx.fxml;
-    requires javafx.controls;
-    requires java.sql;
-    requires mysql.connector.java;
-
-    opens kernel;
-}
