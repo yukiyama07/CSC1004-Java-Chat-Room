d9f73a53c88acc898623fb57efa3ac4798e196c5
Huo Yu <112453676+yukiyama07@users.noreply.github.com>
2023-05-03 00:53:01 +0800

    Delete src directory

--- src/main/java/cn/com/UChat/repository/MessageListRepository.java
+++
@@ -1,9 +0,0 @@
-package cn.com.UChat.repository;
-
-import cn.com.UChat.entity.MessageList;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-// Message list repository
-public interface MessageListRepository extends JpaRepository<MessageList, Long>, JpaSpecificationExecutor {
-}
